e56c094cea78f272358a384009890efa4278e8e6
Alex Rael <alprael22@gmail.com>
2018-10-15 08:27:19 -0600

    added buttons and on click listeners

--- app/src/main/java/com/alprael/readwithoutme/BookActivity.java
+++ app/src/main/java/com/alprael/readwithoutme/BookActivity.java
@@ -2,12 +2,166 @@ package com.alprael.readwithoutme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class BookActivity extends AppCompatActivity {
+
+  private ImageButton book;
+  private Button button;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_book);
+
+    book = (ImageButton) findViewById(R.id.imageButton1);
+    book.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    button = (Button) findViewById(R.id.button1);
+    button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    book = (ImageButton) findViewById(R.id.imageButton2);
+    book.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    button = (Button) findViewById(R.id.button2);
+    button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    book = (ImageButton) findViewById(R.id.imageButton3);
+    book.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    button = (Button) findViewById(R.id.button3);
+    button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    book = (ImageButton) findViewById(R.id.imageButton4);
+    book.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    button = (Button) findViewById(R.id.button4);
+    button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    book = (ImageButton) findViewById(R.id.imageButton5);
+    book.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    button = (Button) findViewById(R.id.button5);
+    button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    book = (ImageButton) findViewById(R.id.imageButton6);
+    book.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    button = (Button) findViewById(R.id.button6);
+    button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    book = (ImageButton) findViewById(R.id.imageButton7);
+    book.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    button = (Button) findViewById(R.id.button7);
+    button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    book = (ImageButton) findViewById(R.id.imageButton8);
+    book.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    button = (Button) findViewById(R.id.button8);
+    button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    book = (ImageButton) findViewById(R.id.imageButton9);
+    book.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
+    button = (Button) findViewById(R.id.button9);
+    button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+
   }
+
+
 }
--- app/src/main/java/com/alprael/readwithoutme/BookFragment.java
+++ app/src/main/java/com/alprael/readwithoutme/BookFragment.java
@@ -2,7 +2,7 @@ package com.alprael.readwithoutme;
 
 import android.support.v4.app.Fragment;
 
-public class ScheduleFragment extends Fragment {
+public class BookFragment extends Fragment {
 
 
 
